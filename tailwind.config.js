d274f49d82e94b67395ae8887d51f60407960c3c
WGB5445 <919603023@qq.com>
2023-09-23 16:53:05 +0000

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import  plugin  from "tailwindcss/plugin";
+
 module.exports = {
   content: ["./templates/**/*.{html,js}"],
   theme: {
@@ -9,19 +12,43 @@ module.exports = {
       xl: '1440px',
     },
     colors: {
-      'blue': '#1fb6ff',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
+      'blue': 'rgb(31 182 255)',
+      'gray': "rgb(133 133 133)",
+      'background-gray':'rgb(211 220 230)'
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
     },
     extend: {
+      typography:{
+        'h1': {
+          "margin": "0",
+          "padding": "0"
+        },
+        'h2': {
+          "margin": "0",
+          "padding": "0"
+        },
+        'h3': {
+          "margin": "0",
+          "padding": "0"
+        },
+        'h4': {
+          "margin": "0",
+          "padding": "0"
+        },
+        'h5': {
+          "margin": "0",
+          "padding": "0"
+        },
+        'h6': {
+          "margin": "0",
+          "padding": "0"
+        },
+      },
+      colors:{
+        "brief-font-color": "rgb(76 73 72)",
+      },
       spacing: {
         '128': '32rem',
         '144': '36rem',
@@ -30,12 +57,23 @@ module.exports = {
         brief: "0.5rem",
       },
       boxShadow:{
-        brief: "0 3px 8px 6px rgba(7,17,27,0.05)",
-        'brief-hover': "0 3px 8px 6px rgba(7,17,27,0.09);"
+        brief: "0 0.1875rem 0.5rem 0.375rem rgba(7,17,27,0.05)"
       },
     }
   },
-  plugins: [],
+  plugins: [ 
+    plugin(function({addBase, theme}){
+      addBase({
+        'h1': {margin: theme('margin.0'),padding: theme('padding.0')},
+        'h2': {margin: theme('margin.0'),padding: theme('padding.0')},
+        'h3': {margin: theme('margin.0'),padding: theme('padding.0')},
+        'h4': {margin: theme('margin.0'),padding: theme('padding.0')},
+        'h5': {margin: theme('margin.0'),padding: theme('padding.0')},
+        'h6': {margin: theme('margin.0'),padding: theme('padding.0')},
+        "p": {margin: theme('margin.0'),padding: theme('padding.0')},
+      })
+    })
+  ],
 
 }
 
